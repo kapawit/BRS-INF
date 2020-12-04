f7d43522fd9a9487d09b78119e92206d5def51d2
fahdiazhannu <zanuazhannu@gmail.com>
2020-12-04 13:59:04 +0700

    ubah variabel

--- simulasi/app/Controllers/Admin/Login.php
+++ simulasi/app/Controllers/Admin/Login.php
@@ -21,18 +21,18 @@ class Login extends BaseController
 
         $session = session();
         $model = new UsersModel();
-        $nim = $this->request->getVar('nim');
+        $nmr_induk = $this->request->getVar('nmr_induk');
         $password = $this->request->getVar('password');
         //akses db user
-        $data = $model->where('nim', $nim)->first();
+        $data = $model->where('nmr_induk', $nmr_induk)->first();
         if ($data) {
             $pass = $data['password']; //data dari db
-            $nm = $data['nm_mhs'];
+            $nama = $data['nama'];
             $verify_pass = password_verify($password, $pass);
             if ($password==$pass) {
                 $sess_data = [
-                    'nim'       => $data['nim'],
-                    'nm_mhs'     => $data['nm_mhs'],
+                    'nmr_induk'       => $data['nmr_induk'],
+                    'nama'     => $data['nama'],
                     'logged_in'     => TRUE
                 ];
                 $session->set($sess_data);
@@ -42,7 +42,7 @@ class Login extends BaseController
                 return redirect()->to(base_url());
             }
         } else {
-            $session->setFlashdata('msg', 'NIM tidak ada');
+            $session->setFlashdata('msg', 'Nomor Induk tidak ada');
             return redirect()->to(base_url());
         }
     }
